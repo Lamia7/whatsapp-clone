c8badcaa45c8a6c0f42933f1d5b06597c59a2d55
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-25 19:24:06 +0100

    ✨ Initialize body app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+//import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    // BEM naming convention (component naming)
+    <div className="app">
+      {/* <h1>Let's build a whatsapp clone</h1> */}
+
+      <div className="app__body">
+        {/* Sidebar on left */}
+        {/* Chat on right */}
+      </div>
     </div>
   );
 }
