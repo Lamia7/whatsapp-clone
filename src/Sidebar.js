2463f08c4b1e9257c2ac9ea78ce8024db8f57fef
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-25 19:52:46 +0100

    🎨 Added sections for sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,6 +6,17 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
+      <div className="sidebar__header">
+
+      </div>
+
+      <div className="sidebar__search">
+
+      </div>
+
+      <div className="sidebar__chats">
+          
+      </div>
     </div>
   );
 }
