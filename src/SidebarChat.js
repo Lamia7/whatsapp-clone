74928982080ca77f50fd77da72068df7758a1db7
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-26 20:41:42 +0100

    ⚗ Testing avatar API

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import './SidebarChat.css'
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import "./SidebarChat.css";
 
 function SidebarChat() {
-    return (
-        <div>
-            <h1>Hey</h1>
-        </div>
-    )
+  return (
+    <div className="sidebarChat">
+        {/* check documentation here : https://avatars.dicebear.com/*/}
+      <Avatar src="https://avatars.dicebear.com/api/female/cindy.svg" />
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>Last message...</p>
+      </div>
+    </div>
+  );
 }
 
-export default SidebarChat
+export default SidebarChat;
