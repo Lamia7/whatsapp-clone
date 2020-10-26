5d03e75af6c44870d6992d77685e88558cdfa63a
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-26 21:36:21 +0100

    ⚗ Testing reusing a prop to add new chat

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
 import "./SidebarChat.css";
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) { // passing prop: addNewChat
   // get random seed whenever the SidebarChat loads
   const [seed, setSeed] = useState("");
 
@@ -10,13 +10,21 @@ function SidebarChat() {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
 
-  return (
+  const createChat = () => {};
+
+  // if it is not the addNewChat, (?) show this, (:) otherwise
+  return !addNewChat ? (
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
         <h2>Room name</h2>
         <p>Last message...</p>
       </div>
+    </div>
+  ) : (
+    // otherwise render out this div
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add new chat</h2>
     </div>
   );
 }
