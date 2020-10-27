5c8580886e916d427f000ddeca7d395aa9e44b9b
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-27 23:49:53 +0100

    ✨ Started chat footer style

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,7 @@
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 import React, { useState, useEffect } from "react";
 import "./Chat.css";
@@ -36,13 +38,19 @@ function Chat() {
       <div className="chat__body">
         <p className={`chat__message ${true && "chat__reciever"}`}>
           <span className="chat__name">Lamia</span>
-          Hello everyone !
-          <span className="chat__timestamp">3:52pm</span>
+          Hello everyone !<span className="chat__timestamp">3:52pm</span>
         </p>
         <p className="chat__message">Hey guys !</p>
       </div>
 
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" />
+          <button>Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
